308cdafd5ef4f35c3ad168b47655542740068872
Moosavi.MirHassan <17009501+mir-moo@users.noreply.github.com>
2023-07-02 14:46:54 +0330

    Update var_let_const.js

--- session-00/var_let_const.js
+++ session-00/var_let_const.js
@@ -11,3 +11,11 @@ let x = 10;
 console.log(x);
 x = x * 250;
 console.log(x)
+
+let num;
+console.log(num);
+console.log(num=110);
+console.log(num ** 2)
+console.log(num / x);
+console.log(x / num);
+console.log(x % num);
