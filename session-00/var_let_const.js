763514069706240073162e209655d7b41a67e314
Moosavi.MirHassan <17009501+mir-moo@users.noreply.github.com>
2023-07-02 14:53:06 +0330

    Update var_let_const.js

--- session-00/var_let_const.js
+++ session-00/var_let_const.js
@@ -19,3 +19,11 @@ console.log(num ** 2)
 console.log(num / x);
 console.log(x / num);
 console.log(x % num);
+
+
+// const
+const c; // Uncaught SyntaxError: Missing initializer in const declaration
+const pi = 3.14;
+console.log(pi)
+pi = 4.023; // Uncaught TypeError: Assignment to constant variable.
+console.log(pi)
