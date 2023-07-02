9f884f4c0e56cd43d915eb8b05bad5bc8fdfe00d
Moosavi.MirHassan <17009501+mir-moo@users.noreply.github.com>
2023-07-02 14:53:42 +0330

    Update var_let_const.js

--- session-00/var_let_const.js
+++ session-00/var_let_const.js
@@ -25,5 +25,5 @@ console.log(x % num);
 // const c; // Uncaught SyntaxError: Missing initializer in const declaration
 const pi = 3.14;
 console.log(pi)
-pi = 4.023; // Uncaught TypeError: Assignment to constant variable.
+// pi = 4.023; // Uncaught TypeError: Assignment to constant variable.
 console.log(pi)
