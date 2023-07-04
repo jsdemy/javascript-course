8b194c7ea0d722710909cc89076bc90a70124084
Moosavi.MirHassan <17009501+mir-moo@users.noreply.github.com>
2023-07-04 13:28:24 +0330

    Create datatypes.js

---
+++ session-00/datatypes.js
@@ -0,0 +1,13 @@
+// js datatypes
+// -----------------------
+// primitive:
+// string - number - boolean 
+// null - undefined
+
+let score = 85;
+const val = 120;
+let firstName = "Ali";
+console.log(firstName)
+
+
+
