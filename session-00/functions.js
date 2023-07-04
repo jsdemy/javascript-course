57114de87e093d18c840b8fd1f8263d22b8ba18a
Moosavi.MirHassan <17009501+mir-moo@users.noreply.github.com>
2023-07-04 15:56:16 +0330

    Create functions.js
    Function Declaration

---
+++ session-00/functions.js
@@ -0,0 +1,13 @@
+// two types of functions
+// ====================================
+// There are 3 ways of writing a function in JavaScript:
+// Function Declaration.
+// Function Expression.
+// Arrow Function.
+
+// Function Declaration
+// A basic javascript function, here we create a function that divides the 1st element by the second element.
+function myFunction(){
+    console.log('i am myFunction')
+}
+
