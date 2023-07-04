902782708beedbbaff4cb7fea42c96fe64beb35f
Moosavi.MirHassan <17009501+mir-moo@users.noreply.github.com>
2023-07-04 15:39:15 +0330

    Update object-part-01.js

--- session-00/object-part-01.js
+++ session-00/object-part-01.js
@@ -1,3 +1,20 @@
 // oop in js
 
-const car = {type:"Fiat", model:"500", color:"white"};
+const car = { type: "Fiat", model: "500", color: "white" };
+
+const p1 = {
+    firstName: 'ali', lastName: 'yazdi', age: 34,
+    chessScors: [45, 65, 88]
+};
+
+const p2 = {
+    firstName: 'hassan', lastName: 'moosavi', age: 33,
+    chessScors: {
+        'Math': 88,
+        'Physics': 95,
+        'Biology': 73
+    }
+};
+
+
+console.log(`${p1.firstName} ${p1.lastName} is ${p1.age} years old`)
