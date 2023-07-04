101968d6fc1d6eb834d618dddc1fa2c5ebf5f958
Moosavi.MirHassan <17009501+mir-moo@users.noreply.github.com>
2023-07-04 17:50:59 +0330

    array object

---
+++ session-00/array_object.js
@@ -0,0 +1,14 @@
+// an array contains objects
+const lessons = [
+    {id: 1, name: 'math', isEnable: true},
+    {id: 2, name: 'chemistry', isEnable: false}
+];
+
+console.log(lessons)
+
+console.log(lessons[0])
+console.log(lessons[1])
+
+console.log(lessons[0].id)
+console.log(lessons[0].name)
+console.log(lessons[0].isEnable)
