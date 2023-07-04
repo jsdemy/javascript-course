a1afe194a6dfc9d17199aff47a0710df7d3ff487
Moosavi.MirHassan <17009501+mir-moo@users.noreply.github.com>
2023-07-04 13:36:09 +0330

    Update datatypes.js

--- session-00/datatypes.js
+++ session-00/datatypes.js
@@ -7,7 +7,14 @@
 let score = 85;
 const val = 120;
 let firstName = "Ali";
-console.log(firstName)
+let lastName = 'Yazdi';
+let isEnabled = true;
+let gender = null;
+let undefinedVar = undefined;
+let floatNumber = 3.14;
+
+console.log(`typeof '${score}' score= ${typeof score}`)
+console.log(`typeof '${score}' score= ${typeof score}`)
 
 
 
