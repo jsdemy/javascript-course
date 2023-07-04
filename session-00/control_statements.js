9af5b0f3a1711c86eef7151e9a66666c3a9c7a98
Moosavi.MirHassan <17009501+mir-moo@users.noreply.github.com>
2023-07-04 17:59:56 +0330

    if, if..else
    if, if..else statements example

---
+++ session-00/control_statements.js
@@ -0,0 +1,19 @@
+// if, if..else
+let n1 = 5;
+let v = 'this is sample';
+
+if(v.length > n1){
+    console.log('yes')
+}
+
+if (n1 == v.length) {
+    console.log(`n1 == "${v}".length`)
+} else {
+    console.log(`n1=${n1} != "${v}".length`)
+}
+
+
+// switch case 
+
+
+// loop
