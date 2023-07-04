9c59d7dacf2117c37affe989c4181a307d29c586
Moosavi.MirHassan <17009501+mir-moo@users.noreply.github.com>
2023-07-04 15:42:43 +0330

    Update object-part-01.js

--- session-00/object-part-01.js
+++ session-00/object-part-01.js
@@ -7,12 +7,12 @@ console.log('===========================')
 
 const p1 = {
     firstName: 'ali', lastName: 'yazdi', age: 34,
-    chessScors: [45, 65, 88]
+    chessScores: [45, 65, 88]
 };
 
 const p2 = {
     firstName: 'hassan', lastName: 'moosavi', age: 33,
-    chessScors: {
+    grades: {
         'Math': 88,
         'Physics': 95,
         'Biology': 73
@@ -21,3 +21,7 @@ const p2 = {
 
 
 console.log(`${p1.firstName} ${p1.lastName} is ${p1.age} years old`)
+
+console.log(p1.chessScors[2])
+
+console.log(`"${p2.firstName} ${p2.lastName}" grade in Physics is: ${p2.grades.Physics}`)
