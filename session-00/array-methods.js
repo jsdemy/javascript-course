2653b54c539603846134a42885019dc193bc53bc
Moosavi.MirHassan <17009501+mir-moo@users.noreply.github.com>
2023-07-05 09:05:10 +0330

    Update array-methods.js

--- session-00/array-methods.js
+++ session-00/array-methods.js
@@ -10,6 +10,7 @@ const courses = [
 ];
 
 
+console.log('----------forEach-----------')
 // forEach() is look like for..of loop
 courses.forEach(function (item) {
     let cn = item.courseName
@@ -19,7 +20,7 @@ courses.forEach(function (item) {
     console.log(`'${cn}' course in '${cd}' hours is '${ca ? 'available' : 'unavailable'}'`)
 })
 
-console.log('---------------------')
+console.log('----------forEach-----------')
 // callback function is an arrow function
 courses.forEach((item) => {
     let cn = item.courseName
@@ -29,4 +30,28 @@ courses.forEach((item) => {
     console.log(`'${cn}' course in '${cd}' hours is '${ca ? 'available' : 'unavailable'}'`)
 })
 
+
+
+console.log('----------map-----------')
 // map()
+const newCourses = courses.map((item) => {
+    return { id: item.id + 100, Name: item.courseName.toUpperCase() }
+})
+
+let list_1 = courses.map((item) => {
+    return item.courseName
+})
+
+console.log(newCourses)
+console.log(list_1)
+
+console.log('----------filter-----------')
+// filter()
+let newList = courses.filter((x) => {
+    return x.isAvailable
+}).map((x) => {
+    return x.courseName
+})
+
+console.log(newList)
+
