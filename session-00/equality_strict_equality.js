b7f946a4c76db51f506d250aba82e39d968920f9
Moosavi.MirHassan <17009501+mir-moo@users.noreply.github.com>
2023-07-05 08:48:43 +0330

    Update equality_strict_equality.js

--- session-00/equality_strict_equality.js
+++ session-00/equality_strict_equality.js
@@ -12,15 +12,3 @@ console.log(obj === str); // false
 console.log(null === undefined); // false
 console.log(obj === null); // false
 console.log(obj === undefined); // false
-
-function add(x = 1, y = 2) {
-    console.log(x + y)
-}
-
-// add(y = 10, x = 25);
-
-// add();
-
-// add(y = 55, x = 3);
-
-add(y = 5) // wow ~>  7
