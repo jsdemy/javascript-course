2c3302cd5f4e3cf8c88f74f1b0498b8004654f69
Moosavi.MirHassan <17009501+mir-moo@users.noreply.github.com>
2023-07-06 22:27:51 +0330

    select elements

--- session-00/js/dom.js
+++ session-00/js/dom.js
@@ -1,1 +1,25 @@
-console.log(window);
+// console.log(window);
+// window.alert('hello world');
+
+// const msg = document.getElementById("msg").innerHTML = 'message <mark>comes here</mark>';
+// const divContainer = document.getElementsByClassName('container');
+// const msg = document.querySelector("#msg").innerHTML = 'message <mark>comes here</mark>';
+// const el = document.querySelector('div');
+// const allDiv = document.querySelectorAll('div');
+
+// console.log(allDiv)
+
+
+const myform = document.querySelector('#my-form')
+const fname = document.querySelector('#fname')
+const lname = document.querySelector('#lname')
+const country = document.querySelector('#country')
+const subject = document.querySelector('#subject')
+const msg = document.querySelector('.msg')
+const contacts_ol = document.querySelector('#contacts-list')
+
+
+
+
+
+
